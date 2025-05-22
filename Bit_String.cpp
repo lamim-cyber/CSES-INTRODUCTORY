ec36a47a94f32cf7aff1e4d978e9a15b89a2a252
Muntakim Unnur Lamim <82176316+lamim-cyber@users.noreply.github.com>
2025-05-22 22:43:10 +0600

    Update Bit_String.cpp

--- Bit_String.cpp
+++ Bit_String.cpp
@@ -5,7 +5,8 @@ int main()
 {
   int n;
   cin>> n;
-  long long sum=1;
+  long long sum=1;// if i take 1<<n, it overflows
+  // so the simpliest way to iterarte
   for(int i=0; i<n; i++)
   {
       sum= (sum*2)%mod;
